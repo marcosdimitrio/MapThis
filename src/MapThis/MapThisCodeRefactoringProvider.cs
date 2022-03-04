472ea32852bbcc757d9153f009949ea8690aa575
Marcos Dimitrio <marcos@dimitrio.com>
2022-03-04 17:55:40 -0300

    Change childMapInformationAlreadyExists to boolean

--- src/MapThis/MapThisCodeRefactoringProvider.cs
+++ src/MapThis/MapThisCodeRefactoringProvider.cs
@@ -55,9 +55,6 @@ namespace MapThis
 
         private async Task<Document> ReplaceAsync(CodeRefactoringContext context, MethodDeclarationSyntax methodSyntax, CancellationToken cancellationToken)
         {
-            //var semanticModel = await context.Document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            //var methodSymbol = semanticModel.GetDeclaredSymbol(methodSyntax, cancellationToken);
-
             var mapInformation = await GetMapInformation(context, methodSyntax, cancellationToken).ConfigureAwait(false);
 
             var blocks = GetBlocks(methodSyntax, mapInformation);
@@ -65,6 +62,119 @@ namespace MapThis
             var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
             var newRoot = root.ReplaceNode(methodSyntax, blocks);
             return context.Document.WithSyntaxRoot(newRoot);
+        }
+
+        private static async Task<MapInformationDto> GetMapInformation(CodeRefactoringContext context, MethodDeclarationSyntax methodSyntax, CancellationToken cancellationToken)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var semanticModel = await context.Document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var methodSymbol = semanticModel.GetDeclaredSymbol(methodSyntax, cancellationToken);
+            //var generator = SyntaxGenerator.GetGenerator(context.Document);
+
+            var accessModifiers = methodSyntax.Modifiers.ToList();
+
+            var firstParameterSymbol = methodSymbol.Parameters[0];
+            var targetType = methodSymbol.ReturnType;
+
+            var sourceMembers = firstParameterSymbol.Type.GetPublicProperties();
+            var targetMembers = targetType.GetPublicProperties();
+
+            var existingMethods = root
+                .DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Select(x => semanticModel.GetDeclaredSymbol(x, cancellationToken))
+                .ToList();
+
+            var mapInformation = GetMap(accessModifiers, firstParameterSymbol.Type, targetType, firstParameterSymbol.Name, sourceMembers, targetMembers, existingMethods);
+
+            return mapInformation;
+        }
+
+        private static MapInformationDto GetMap(IList<SyntaxToken> accessModifiers, ITypeSymbol sourceType, ITypeSymbol targetType, string firstParameterName, IList<IPropertySymbol> sourceMembers, IList<IPropertySymbol> targetMembers, IList<IMethodSymbol> existingMethods)
+        {
+            var childrenMapInformation = new List<MapInformationDto>();
+            var childrenMapCollectionInformation = new List<MapCollectionInformationDto>();
+
+            var propertiesToMap = new List<PropertyToMapDto>();
+
+            foreach (var targetProperty in targetMembers)
+            {
+                var sourceProperty = FindPropertyInSource(targetProperty, sourceMembers);
+
+                AssignmentExpressionSyntax newExpression = null;
+                INamedTypeSymbol targetListType = null;
+                INamedTypeSymbol sourceListType = null;
+
+                if (targetProperty.Type.IsSimpleType())
+                {
+                    newExpression = GetNewDirectConversion(firstParameterName, targetProperty.Name);
+                }
+                else if (
+                    targetProperty.Type is INamedTypeSymbol targetNamedType && targetNamedType.IsCollection() &&
+                    sourceProperty?.Type is INamedTypeSymbol sourceNamedType && sourceNamedType.IsCollection()
+                )
+                {
+                    var childMapCollectionAlreadyExists = existingMethods.Any(x =>
+                        SymbolEqualityComparer.Default.Equals(x.ReturnType, targetNamedType) &&
+                        SymbolEqualityComparer.Default.Equals(x.Parameters.FirstOrDefault()?.Type, sourceNamedType)
+                    );
+
+                    if (!childMapCollectionAlreadyExists)
+                    {
+                        targetListType = (INamedTypeSymbol)targetNamedType.GetElementType();
+                        sourceListType = (INamedTypeSymbol)sourceNamedType.GetElementType();
+
+                        var sourceListMembers = sourceListType.GetPublicProperties();
+                        var targetListMembers = targetListType.GetPublicProperties();
+
+                        var privateAccessModifiers = GetNonPublicAccessModifiers(accessModifiers);
+
+                        var childMapCollection = GetMapsForCollection(privateAccessModifiers, sourceProperty.Type, targetProperty.Type, "source", existingMethods);
+                        childrenMapCollectionInformation.AddRange(childMapCollection);
+                    }
+
+                    newExpression = GetConversionWithMap(firstParameterName, targetProperty.Name);
+                }
+                else
+                {
+                    throw new NotSupportedException("Cannot determine target property type");
+                }
+
+                var propertyToMap = new PropertyToMapDto(sourceProperty, targetProperty, newExpression);
+
+                propertiesToMap.Add(propertyToMap);
+            }
+
+            var mapInformation = new MapInformationDto(accessModifiers, firstParameterName, propertiesToMap, sourceType, targetType, childrenMapInformation, childrenMapCollectionInformation);
+
+            return mapInformation;
+        }
+
+        private static IList<MapCollectionInformationDto> GetMapsForCollection(IList<SyntaxToken> accessModifiers, ITypeSymbol sourceType, ITypeSymbol targetType, string firstParameterName, IList<IMethodSymbol> existingMethods)
+        {
+            var newExpression = GetCollectionMapExpression(sourceType, targetType, firstParameterName);
+
+            var sourceListType = (INamedTypeSymbol)sourceType.GetElementType();
+            var targetListType = (INamedTypeSymbol)targetType.GetElementType();
+
+            var sourceListMembers = sourceListType.GetPublicProperties();
+            var targetListMembers = targetListType.GetPublicProperties();
+
+            var childMapInformationAlreadyExists = existingMethods.Any(x =>
+                SymbolEqualityComparer.Default.Equals(x.ReturnType, targetListType) &&
+                SymbolEqualityComparer.Default.Equals(x.Parameters.FirstOrDefault()?.Type, sourceListType)
+            );
+
+            MapInformationDto childMapInformation = null;
+
+            if (!childMapInformationAlreadyExists)
+            {
+                childMapInformation = GetMap(accessModifiers, sourceListType, targetListType, "item", sourceListMembers, targetListMembers, existingMethods);
+            }
+
+            var mapCollectionInformationDto = new MapCollectionInformationDto(accessModifiers, firstParameterName, sourceType, targetType, newExpression, childMapInformation);
+
+            return new List<MapCollectionInformationDto>() { mapCollectionInformationDto };
         }
 
         private IList<MethodDeclarationSyntax> GetBlocks(MethodDeclarationSyntax methodSyntax, MapInformationDto mapInformation)
@@ -152,152 +262,9 @@ namespace MapThis
             foreach (var childMapInformation in mapInformation.ChildrenMapInformation)
             {
                 blocks.AddRange(GetBlocks(null, childMapInformation));
-                //var statement = GetMappedObjectStatement(childMapInformation);
-
-                //var blockSyntax =
-                //    MethodDeclaration(
-                //        IdentifierName(childMapInformation.TargetType.Name),
-                //        Identifier("Map")
-                //    )
-                //    .WithModifiers(
-                //        TokenList(childMapInformation.AccessModifiers)
-                //    )
-                //    .WithParameterList(
-                //        ParameterList(
-                //            SingletonSeparatedList(
-                //                Parameter(Identifier("item"))
-                //                    .WithType(IdentifierName(childMapInformation.SourceType.Name))
-                //            )
-                //        )
-                //    )
-                //    .WithBody(
-                //        Block(
-                //            statement,
-                //            EmptyStatement()
-                //                .WithSemicolonToken(
-                //                    MissingToken(TriviaList(), SyntaxKind.SemicolonToken, TriviaList(CarriageReturnLineFeed, CarriageReturnLineFeed, Whitespace(new string(' ', methodSyntax.GetLeadingTrivia().FullSpan.Length + 4))))
-                //                ),
-                //            ReturnStatement(IdentifierName("newItem"))
-                //        )
-                //    );
-
-                //blocks.Add(blockSyntax);
             }
 
             return blocks;
-        }
-
-        private static async Task<MapInformationDto> GetMapInformation(CodeRefactoringContext context, MethodDeclarationSyntax methodSyntax, CancellationToken cancellationToken)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var semanticModel = await context.Document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            var methodSymbol = semanticModel.GetDeclaredSymbol(methodSyntax, cancellationToken);
-            //var generator = SyntaxGenerator.GetGenerator(context.Document);
-
-            var accessModifiers = methodSyntax.Modifiers.ToList();
-
-            var firstParameterSymbol = methodSymbol.Parameters[0];
-            var targetType = methodSymbol.ReturnType;
-
-            var sourceMembers = firstParameterSymbol.Type.GetPublicProperties();
-            var targetMembers = targetType.GetPublicProperties();
-
-            var existingMethods = root
-                .DescendantNodes()
-                .OfType<MethodDeclarationSyntax>()
-                .Select(x => semanticModel.GetDeclaredSymbol(x, cancellationToken))
-                .ToList();
-
-            var mapInformation = GetMap(accessModifiers, firstParameterSymbol.Type, targetType, firstParameterSymbol.Name, sourceMembers, targetMembers, existingMethods);
-
-            return mapInformation;
-        }
-
-        private static MapInformationDto GetMap(IList<SyntaxToken> accessModifiers, ITypeSymbol sourceType, ITypeSymbol targetType, string firstParameterName, IList<IPropertySymbol> sourceMembers, IList<IPropertySymbol> targetMembers, IList<IMethodSymbol> existingMethods)
-        {
-            var childrenMapInformation = new List<MapInformationDto>();
-            var childrenMapCollectionInformation = new List<MapCollectionInformationDto>();
-
-            var propertiesToMap = new List<PropertyToMapDto>();
-
-            foreach (var targetProperty in targetMembers)
-            {
-                var sourceProperty = FindPropertyInSource(targetProperty, sourceMembers);
-
-                AssignmentExpressionSyntax newExpression = null;
-                INamedTypeSymbol targetListType = null;
-                INamedTypeSymbol sourceListType = null;
-
-                if (targetProperty.Type.IsSimpleType())
-                {
-                    newExpression = GetNewDirectConversion(firstParameterName, targetProperty.Name);
-                }
-                else if (
-                    targetProperty.Type is INamedTypeSymbol targetNamedType && targetNamedType.IsCollection() &&
-                    sourceProperty?.Type is INamedTypeSymbol sourceNamedType && sourceNamedType.IsCollection()
-                )
-                {
-                    var childMapCollectionAlreadyExists = existingMethods.Where(x =>
-                        SymbolEqualityComparer.Default.Equals(x.ReturnType, targetNamedType) &&
-                        SymbolEqualityComparer.Default.Equals(x.Parameters.FirstOrDefault()?.Type, sourceNamedType)
-                    ).ToList();
-
-                    if (childMapCollectionAlreadyExists.Count == 0)
-                    {
-                        targetListType = (INamedTypeSymbol)targetNamedType.GetElementType();
-                        sourceListType = (INamedTypeSymbol)sourceNamedType.GetElementType();
-
-                        var sourceListMembers = sourceListType.GetPublicProperties();
-                        var targetListMembers = targetListType.GetPublicProperties();
-
-                        var privateAccessModifiers = GetNonPublicAccessModifiers(accessModifiers);
-
-                        var childMapCollection = GetMapsForCollection(privateAccessModifiers, sourceProperty.Type, targetProperty.Type, "source", existingMethods);
-                        childrenMapCollectionInformation.AddRange(childMapCollection);
-                    }
-
-                    newExpression = GetConversionWithMap(firstParameterName, targetProperty.Name);
-                }
-                else
-                {
-                    throw new NotSupportedException("Cannot determine target property type");
-                }
-
-                var propertyToMap = new PropertyToMapDto(sourceProperty, targetProperty, newExpression);
-
-                propertiesToMap.Add(propertyToMap);
-            }
-
-            var mapInformation = new MapInformationDto(accessModifiers, firstParameterName, propertiesToMap, sourceType, targetType, childrenMapInformation, childrenMapCollectionInformation);
-
-            return mapInformation;
-        }
-
-        private static IList<MapCollectionInformationDto> GetMapsForCollection(IList<SyntaxToken> accessModifiers, ITypeSymbol sourceType, ITypeSymbol targetType, string firstParameterName, IList<IMethodSymbol> existingMethods)
-        {
-            var newExpression = GetCollectionMapExpression(sourceType, targetType, firstParameterName);
-
-            var sourceListType = (INamedTypeSymbol)sourceType.GetElementType();
-            var targetListType = (INamedTypeSymbol)targetType.GetElementType();
-
-            var sourceListMembers = sourceListType.GetPublicProperties();
-            var targetListMembers = targetListType.GetPublicProperties();
-
-            var childMapInformationAlreadyExists = existingMethods.Where(x =>
-                SymbolEqualityComparer.Default.Equals(x.ReturnType, targetListType) &&
-                SymbolEqualityComparer.Default.Equals(x.Parameters.FirstOrDefault()?.Type, sourceListType)
-            ).ToList();
-
-            MapInformationDto childMapInformation = null;
-
-            if (childMapInformationAlreadyExists.Count == 0)
-            {
-                childMapInformation = GetMap(accessModifiers, sourceListType, targetListType, "item", sourceListMembers, targetListMembers, existingMethods);
-            }
-
-            var mapCollectionInformationDto = new MapCollectionInformationDto(accessModifiers, firstParameterName, sourceType, targetType, newExpression, childMapInformation);
-
-            return new List<MapCollectionInformationDto>() { mapCollectionInformationDto };
         }
 
         private static StatementSyntax GetCollectionMapExpression(ITypeSymbol sourceType, ITypeSymbol targetType, string firstParameterName)
